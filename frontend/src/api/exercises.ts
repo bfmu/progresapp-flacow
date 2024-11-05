ef95953cf5a2d25f34791f0b014e37e2e801c14e
Bryan Munoz <swsbmm@gmail.com>
2024-11-05 12:03:41 -0500

    feat: new service for backend exercises request

--- frontend/src/api/exercises.ts
+++ frontend/src/api/exercises.ts
@@ -1,6 +1,17 @@
 import apiClient from "../lib/axios";
 
 export const getAllExercises = async () => {
-  const response = await apiClient.get("/exercises");
-  return response.data;
+  return await apiClient.get("/exercises");
+};
+
+export const updateExercises = async (id: number, values: any) => {
+  return await apiClient.patch(`/exercises/${id}`, values);
+};
+
+export const createExercises = async (values: any) => {
+  return await apiClient.post(`/exercises`, values);
+};
+
+export const deleteExercises = async (id: number) => {
+  return await apiClient.delete(`/exercises/${id}`);
 };
