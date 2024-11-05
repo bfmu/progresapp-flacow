5c15ab886741289c074ad0074b9235b1a7632ccf
Bryan Munoz <swsbmm@gmail.com>
2024-11-05 16:10:16 -0500

    refactor: navbar outside container

--- frontend/src/react/pages/Dashboard.tsx
+++ frontend/src/react/pages/Dashboard.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import ExerciseListDashboard from "../components/ExercisesListDashboard";
-import {Button, Container} from '@mui/material';
+import { Button, Container } from "@mui/material";
 
 export default function Dashboard() {
   return (
-    <Container>
+    <>
       <Navbar />
-      <ExerciseListDashboard />
-    </Container>
+      <Container>
+        <ExerciseListDashboard />
+      </Container>
+    </>
   );
 }
