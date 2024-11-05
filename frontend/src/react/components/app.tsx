5cb2ba55629793d216218c184e53ffcdc7137622
Bryan Munoz <swsbmm@gmail.com>
2024-11-05 12:05:15 -0500

    feat: added new route for exercises administration and new provider for notiStack

--- frontend/src/react/components/app.tsx
+++ frontend/src/react/components/app.tsx
@@ -7,14 +7,15 @@ import Dashboard from "../pages/Dashboard";
 import Profile from "./Profile";
 import ProtectedRoute from "./ProtectedRoute";
 import { useAuthStore } from "../../store/auth";
+import { SnackbarProvider } from "notistack";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import { CssBaseline } from "@mui/material";
 import Muscles from "../pages/Muscles";
-
+import Exercises from "../pages/Exercises";
 
 export const App = () => {
   const isAuth = useAuthStore((state) => state.isAuth);
@@ -31,6 +32,7 @@ export const App = () => {
             { path: "profile", element: <Profile /> },
             { path: "dashboard", element: <Dashboard /> },
             { path: "muscles", element: <Muscles /> },
+            { path: "exercises", element: <Exercises /> },
           ],
         },
       ],
@@ -38,8 +40,10 @@ export const App = () => {
   ]);
   return (
     <React.StrictMode>
-      <CssBaseline/>
-      <RouterProvider router={router} />{" "}
+      <SnackbarProvider maxSnack={5} autoHideDuration={3000}>
+        <CssBaseline />
+        <RouterProvider router={router} />{" "}
+      </SnackbarProvider>
     </React.StrictMode>
   );
 };
