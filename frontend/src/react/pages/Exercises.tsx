0b400900a7f316d70be691d8c3fb2b7dbd70d90a
Bryan Munoz <swsbmm@gmail.com>
2024-11-05 12:10:47 -0500

    feat: new page in the application for the exercises

---
+++ frontend/src/react/pages/Exercises.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ExerciseList from "../components/ExerciseList";
+import Navbar from "../components/Navbar";
+
+export default function Exercises() {
+  return (
+    <>
+      <Navbar />
+      <ExerciseList />
+    </>
+  );
+}
